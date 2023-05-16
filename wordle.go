574d36060d6c05ae4e9a69caca021028d423e50b
Julie Anzaroot <julieanzaroot@gmail.com>
2023-05-16 17:15:55 -0400

    Completed all functions

--- wordle.go
+++ wordle.go
@@ -1,1 +1,158 @@
 package wordle
+
+import (
+	"errors"
+	//"fmt"
+	"github.com/j-zarr/wordle/words"
+)
+
+const (
+	maxGuesses = 6
+	wordSize   = 5
+)
+
+type wordleState struct {
+	// word is the word that the user is trying to guess
+	word [wordSize]byte
+	// guesses holds the guesses that the user has made
+	guesses [maxGuesses]guess
+	// currGuess is the index of the available slot in guesses
+	currGuess int
+}
+
+// guess is an attempt to guess the word
+type guess [wordSize]letter
+
+type letter struct {
+	// char is the letter that this struct represents
+	char byte
+	// status is the state of the letter (absent, present, correct)
+	status letterStatus
+}
+
+// letterStatus can be none, correct, present, or absent
+type letterStatus int
+
+const (
+	// none = no status, not guessed yet
+	none letterStatus = iota
+	// absent = not in the word
+	absent
+	// present = in the word, but not in the correct position
+	present
+	// correct = in the correct position
+	correct
+)
+
+// newWordleState builds a new wordleState from a string.
+// Pass in the word you want the user to guess.
+func newWordleState(word string) wordleState {
+	w := wordleState{}
+	wordChars := []byte{}
+
+	for i := range word {
+		wordChars = append(wordChars, word[i])
+	}
+
+	w.word = [5]byte(wordChars)
+
+	return w
+}
+
+func newLetter(char byte) letter {
+	return letter{char: char, status: none}
+}
+
+func newGuess(str string) guess {
+
+	var g guess
+
+	for i, c := range str {
+		g[i] = newLetter(byte(c)) //letter = {0,0}  guess = [{0,0}, {0,0}, {0,0}, {0,0}]
+	}
+	return g
+}
+
+// updateLettersWithWord updates the status of the letters in the guess based on a word
+func (g *guess) updateLettersWithWord(word [wordSize]byte) {
+	// g array of letter , letter {char byte, status letterStatus}
+	for i := range g {
+		letter := &g[i]
+		if letter.char == word[i] {
+			letter.status = correct
+		} else {
+			for _, b := range word {
+				if letter.char == b {
+					letter.status = present
+				}
+			}
+
+		}
+		if letter.status == 0 {
+			letter.status = absent
+		}
+	}
+
+}
+
+func (g *guess) string() string {
+	str := ""
+	for _, l := range g {
+		if 'A' <= l.char && l.char <= 'Z' {
+			str += string(l.char)
+		}
+	}
+	return str
+}
+
+// appendGuess adds a guess to the wordleState. It returns an error
+// if the guess is invalid.
+func (w *wordleState) appendGuess(g guess) error {
+
+	w.currGuess ++
+
+	var err error
+
+	if w.currGuess > 5 {
+		err = errors.New("maximum number of guesses reached")
+
+	} else if len(g.string()) != 5 {
+		err = errors.New("invalid guess - guess not correct length (5)")
+		
+	} else {
+		// for _, l := range g {
+			// if l.char < 'A' || l.char > 'Z' {
+			if !words.IsWord(g.string()) {
+				err = errors.New("invalid guess - invalid word")
+			}
+			// }
+		//}
+	}
+
+	if err != nil {
+		return err
+	}
+
+	w.guesses[w.currGuess] = g
+
+	return err
+}
+
+
+// isWordGuessed returns true when the latest guess is the correct word
+func (w *wordleState) isWordGuessed() bool {
+
+	var b [5]byte
+
+	for i, l := range w.guesses[w.currGuess] {
+		b[i] = l.char
+	}
+
+	return  b == w.word
+}
+
+func (w *wordleState) shouldEndGame() bool {
+
+	return w.isWordGuessed() || w.currGuess >= 6
+}
+
--- wordle_test.go
+++ wordle_test.go
@@ -1,1 +1,123 @@
 package wordle
+
+import "testing"
+
+func TestNewWordleState(t *testing.T) {
+	word := "HELLO"
+	ws := newWordleState(word)
+	wordleAsString := string(ws.word[:])
+
+	t.Log("Created wordleState:")
+	t.Logf("    word: %s", wordleAsString)
+	t.Logf("    guesses: %v", ws.guesses)
+	t.Logf("    currGuess: %v", ws.currGuess)
+
+	if wordleAsString != word {
+		t.Errorf("Expected word to be %s, got %s", wordleAsString, word)
+	}
+}
+
+func statusToString(status letterStatus) string {
+	switch status {
+	case none:
+		return "none"
+	case correct:
+		return "correct"
+	case present:
+		return "present"
+	case absent:
+		return "absent"
+	default:
+		return "unknown"
+	}
+}
+
+
+// // string converts a guess into a string
+// func (g *guess) string() string {
+//     str := ""
+//     for _, l := range g {
+//         if 'A' <= l.char && l.char <= 'Z' {
+//             str += string(l.char)
+//         }
+//     }
+//     return str
+// }
+
+func TestNewGuess(t *testing.T) {
+	wordToGuess := "YIELD"
+	guess := newGuess(wordToGuess)
+
+	t.Logf("New guess: %s", guess.string())
+
+	// Check that the letter and status are correct
+	for i, l := range guess {
+		t.Logf("    letter %d: %c, %s", i, l.char, statusToString(l.status))
+
+		if l.char != wordToGuess[i] || l.status != none {
+			t.Errorf(
+				"letter[%d] = %c, %s; want %c, none",
+				i,
+				l.char,
+				statusToString(l.status),
+				wordToGuess[i],
+			)
+		}
+	}
+}
+
+
+func TestUpdateLettersWithWord(t *testing.T) {
+	guessWord := "YIELD"
+	guess := newGuess(guessWord)
+
+	var word [wordSize]byte
+	copy(word[:], "HELLO")
+	guess.updateLettersWithWord(word)
+
+	statuses := []letterStatus{
+		absent,  // "Y" is not in "HELLO"
+		absent,  // "I" is not in "HELLO"
+		present, // "E" is in "HELLO" but not in the correct position
+		correct, // "L" is in "HELLO" and in the correct position
+		absent,  // "D" is not in "HELLO"
+	}
+
+	// Check that statuses are correct //letterSatus
+	for i := range guess {
+		if guess[i].status == statuses[i]{
+			t.Logf("Success. Expected %d, Received %d", guess[i].status, statuses[i])
+		} else {
+			t.Errorf("Expected %d, but got %d", guess[i].status, statuses[i])
+		}
+	}
+}
+
+
+func TestIsWordGuessed(t *testing.T) {
+	ws := newWordleState("HELLO")
+	g := newGuess("HELLO")
+
+	g.updateLettersWithWord(ws.word)
+	ws.appendGuess(g)
+
+	if !ws.isWordGuessed() {
+		t.Errorf("isWordGuessed() should return true")
+	}
+}
+
+func TestShouldEndGame(t *testing.T) {
+	ws := newWordleState("HELLO")
+	g := newGuess("HELLO")
+
+	g.updateLettersWithWord(ws.word)
+	ws.appendGuess(g)
+
+	if ws.shouldEndGame() {
+		t.Log("game over")
+	}
+
+	if !ws.shouldEndGame() { 
+		t.Error("isWordGuessed() should return true")
+	}
+}
